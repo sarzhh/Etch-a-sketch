ce6acb087c9ef2594c326815732e73c8a8bd23e4
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-02-11 12:55:13 +0000

    Adds hovering effect to the board

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for(let i = 0; i < 16 * 16; i++){
     cell.classList.add('cell');
     board.appendChild(cell);
 }
+
+const cells = document.querySelectorAll('.cell')
+cells.forEach(cell =>{
+    cell.addEventListener('mouseenter', () => {
+        cell.style.backgroundColor = 'black';
+    })
+})
