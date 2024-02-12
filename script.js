355e42b67740c2dd956ed285e17e6b33dbb4f90d
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-02-12 17:47:38 +0000

    Adds button to changegrid size

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const board = document.querySelector('.board');
+const gridSizeElement = document.querySelector('.gridsize');
 
-for(let i = 0; i < 16 * 16; i++){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    board.appendChild(cell);
+
+const createBoard = (size) => {
+    board.innerHTML = '';
+
+    for(let i = 0; i < size * size; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        board.appendChild(cell);
+    }
+
+    const cells = document.querySelectorAll('.cell');
+
+    const cellSize = 100 / size;
+    cells.forEach(cell => {
+        cell.style.flexBasis = `${cellSize}%`;
+    })
+    
+    
+    cells.forEach(cell =>{
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        })
+    })
+
+
+}
+createBoard(16);
+
+
+const gridSize = () => {
+    gridSizeElement.addEventListener('click', () => {
+     const gridNumber = prompt('Enter Size of the grid');
+     if (gridNumber >= 2 && gridNumber <= 100){
+        createBoard(gridNumber);
+        }
+
+    else (alert("Grid size should be between 1 and 100"));
+    })
+
+    
 }
 
-const cells = document.querySelectorAll('.cell')
-cells.forEach(cell =>{
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'black';
-    })
-})
+gridSize();
